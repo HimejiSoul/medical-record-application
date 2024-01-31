236146ade86d4294c222f84a0a245ec95df003fa
HimejiSoul <perseushilmy@gmail.com>
2024-01-31 11:35:26 +0700

    feat: add layout page

--- src/app/keluargaBersama/page.jsx
+++ src/app/keluargaBersama/page.jsx
@@ -1,14 +1,13 @@
 import Sidebar from "../../components/sidebar";
 import Rightbar from "../../components/rightbar";
-import SearchBar from "../../components/searchBar";
 
 export default function Dashboard() {
 	return (
 		<div className="grid grid-cols-5">
 			<Sidebar />
 			<div className="flex flex-col bg-white col-span-3 py-6 px-3 gap-6">
-				<div>
-					<SearchBar className="mb-10" />
+				<div className="h-14 rounded-xl bg-[#D0E4FF] text-[#6F90BA] font-medium flex items-center justify-center">
+					Ceritanya Search Bar tapi di KB
 				</div>
 				<div className="h-full rounded-xl bg-[#D0E4FF]"></div>
 			</div>
